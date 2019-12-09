62f1c14a96f6ec9ca22330e9bc4ee460e4674ca2
Tarandeep Singh <tarandeep.singh@salesforce.com>
2019-12-10 04:16:20 +0530

    Code Enhancement

--- lwc-services.config.js
+++ lwc-services.config.js
@@ -8,6 +8,6 @@ module.exports = {
         customConfig: './src/server/index.js'
     },
     devServer: {
-        proxy: { '/': 'http://localhost:3002' }
+        proxy: { '/': 'http://localhost:3001' }
     }
 };
---
+++ node_modules/lwc-services/node_modules/@lwc/jest-preset/README.md
@@ -0,0 +1,5 @@
+# @lwc/jest-preset
+
+This is a _temporary_ repo till this pull request is merged into @lwc/jest-preset.
+
+https://github.com/salesforce/lwc-test/pull/36
